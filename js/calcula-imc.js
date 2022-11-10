21ae5ca5fc0049aae3299908187c3860afff4005
juliocesar2222 <116820860+juliocesar2222@users.noreply.github.com>
2022-11-10 07:37:54 -0300

    Update calcula-imc.js

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -1,5 +1,5 @@
 var titulo = document.querySelector(".titulo");
-titulo.textContent = "Aparecida Nutricionista";
+titulo.textContent = "JuliobyMachonutricionista";
 
 var pacientes = document.querySelectorAll(".paciente");
 
